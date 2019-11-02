5ac2aa4032f66607a864c547d9833ebe8c8f0cb1
billy <felicite.lordon@gmail.com>
2019-11-02 12:31:40 +0100

    apply gitignore

--- russia/build/release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/billy/ethz/AlgoCode/russia
-# Build directory: /home/billy/ethz/AlgoCode/russia/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- week5/planks/build/release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/billy/ethz/AlgoCode/week5/planks
-# Build directory: /home/billy/ethz/AlgoCode/week5/planks/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
