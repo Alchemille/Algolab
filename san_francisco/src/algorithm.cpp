f1989fd108c3ecdbf94b2b9a864d94920a4e7474
billy <felicite.lordon@gmail.com>
2019-12-20 15:01:39 +0100

    Simplify san francisco

--- san_francisco/src/algorithm.cpp
+++ san_francisco/src/algorithm.cpp
@@ -14,12 +14,10 @@ using namespace std;
 /*
 Idea: 
 Dynamic programming. 
-DP[i, j] is a pair (score, steps) a player can get with i remaining turns and starting from node j.
-The DP is solved top-down.
-A 2-steps adj-list is built to know which edge a player can get in her next turn.
-The recurrence is basically
-DP[i + 1, j] = max_over_2_steps_neighbors_n (DP(i, n))
-If this value is bigger than m, the number of steps required to get it is returned too.
+DP[i, j] the score a player can get with i remaining turns and starting from node j.
+Important: Seems need to use bottom up to stop incrementing steps used if the score is reached
+before the max number of steps, for starting node 0.
+Other trick allowed by bottom up: if X is cul-de-sac, set DP[i, X] = DP[i, X] which is computed before
 */
 
 void print_dp(vector<vector<pair<long, long>>>& dp) {
@@ -40,7 +38,7 @@ void testcase() {
 
     vector<vector<pair<long, long>>> adj_list(n, vector<pair<long, long>>());
     vector<vector<pair<long, long>>> adj_list2(n, vector<pair<long, long>>());
-    vector<vector<pair<long, long>>> dp(k + 1, vector<pair<long, long>>(n, {-1, -1}));
+    vector<vector<long>> dp(k + 1, vector<long>(n));
     
     // build adj list
     for (long i = 0; i < m; i ++) {
@@ -53,18 +51,15 @@ void testcase() {
     // fill dp
     // base case
     for (long i = 0; i < n; i ++) { 
-        dp[0][i] = {0, 0};
+        dp[0][i] = 0;
     }
 
     long stop = std::numeric_limits<long>::max();
-    long stop_value = 0;
 
     for (long i = 1; i < k + 1; i ++) {
         for (long j = 0; j < n; j ++) {
 
             long res_score = 0;
-            long res_rounds = i;
-            //cout << "recursive call " << rounds_left << " " << node << "\n";
 
             // case where node is cul-de-sac
             if (adj_list[j].size() == 0) {
@@ -75,33 +70,28 @@ void testcase() {
                 for (pair<long, long> neighbor : adj_list[j]) {
 
 
-                    long neighbor_score = dp[i - 1][neighbor.first].first;
-                    long neighbor_rounds = dp[i - 1][neighbor.first].second;
-
+                    long neighbor_score = dp[i - 1][neighbor.first];
                     res_score = max(res_score, neighbor_score + neighbor.second);
                 }
-                dp[i][j] = {res_score, res_rounds};
+                dp[i][j] = res_score;
 
-                if (dp[i][j].first >= x && j == 0) {
-                    stop = min (stop, i);
-                    stop_value = dp[i][j].first;
+                if (dp[i][j] >= x && j == 0) {
+                    stop = min(stop, i);
                 }
             }
         }
     }
 
-    long final_score = dp[k][0].first;
-    long final_rounds = dp[k][0].second;
+    long final_score = dp[k][0];
 
     //print_dp(dp);
 
     // display result
-    cerr << stop << " " << stop_value << " " << final_score << " " << final_rounds << x << "\n";
     if (stop < std::numeric_limits<long>::max()) {
         cout << stop << "\n";
     }
     else if (final_score >= x) {
-        cout << final_rounds << "\n";
+        cout << k << "\n";
     }
     else cout << "Impossible\n";
 }
